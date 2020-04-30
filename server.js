48136dc326216607effc7bdb7d6b5ab1cfb99c51
Andrey Razuvaev <razuvaev@cmsplanet.ru>
2020-04-30 21:01:02 +0300

    prefix config

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const HOST = process.env.HOST
 const PORT = process.env.PORT || 6060
+const PREFIX = process.env.PREFIX
 
 const app = require('./app')
 
 // Here is a strange problem with windows 10 (listen EACCES: permission denied localhost)
+
+const server = require('express')
+
+if (PREFIX) {
+    server.use(PREFIX, app)
+} else {
+    server.use(app)
+}
+
 if (HOST) {
-    app.listen(HOST, PORT, () => {
+    server.listen(HOST, PORT, () => {
         console.log(`File server is listening on ${HOST}:${PORT}`)
     })
 } else {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`File server is listening on port ${PORT}`)
     })
 }
